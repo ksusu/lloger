5167181aa2f84fa9ba1afd7bc74a120db2f30f12
holywater <holywater@gist.ac.kr>
2018-04-13 14:05:13 +0900

    Authorization success advanced

--- llogger_server.js
+++ llogger_server.js
@@ -5,24 +5,22 @@ var index = 1;
 
 var server = http.createServer(function(req, res){
 	if (req.method == 'POST') {
-		
-		var header = req.headers['authorization'];        // get the header
+		console.log(index++ + " : POST");
+
+		var header = req.headers['authorization'];       	 	// get the header
 		console.log("Authorization Header is: ", header);
 
-  		var token = header.split(' ');
-  		            // and the encoded auth token
+  		var token = header.split(' ');            				// and the encoded auth token
      	var buf = new Buffer(token[1], 'base64').toString();    // convert from base64
-      	var parts = buf.split(':');                          // split on colon
+      	var parts = buf.split(':');                          	// split on colon
 
       	var username = parts[0];
       	var password = parts[1];
-		/*console.log(index++ + " : POST");
-		haeder = req.headers['authorization'];
-		token = header.split(/\s+/).pop();
-		auth = (new Buffer(token, 'base64').toString()).split(/:/);*/
-		console.log(parts);
-		console.log(req.url);
-		console.log(url.parse(req.url, true).query);
+
+		console.log('parts : ' + parts);
+		console.log('ID : ' + username + '/ PW : ' + password);
+		console.log('url : ' + req.url);
+		console.log('query : ' + url.parse(req.url, true).query);
 		console.log('Server Running at http://127.0.0.1:8080');
 	}
 	res.end();
